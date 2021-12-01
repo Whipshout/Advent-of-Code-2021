4cf47e69f08aee9d325cfabe9b6f5598a20d7f06
Eduardo Sánchez <whipshout@gmail.com>
2021-12-01 23:54:11 +0100

    Added tests for read_file

--- tools/src/io/read_file.rs
+++ tools/src/io/read_file.rs
@@ -9,3 +9,40 @@ pub fn read_file(path: &str) -> Result<String, std::io::Error> {
 
     Ok(String::from_utf8(file_contents).expect("Error parsing file content"))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::remove_file;
+    use std::io::Write;
+
+    fn create_and_populate_file(text: &str, path: &str) -> Result<(), std::io::Error> {
+        let mut buffer = File::create(path)?;
+        buffer.write_all(text.as_bytes())?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn read_file_works_with_correct_path() {
+        let path = "test_file.txt";
+        let text = "Hello world!";
+
+        create_and_populate_file(text, path).expect("Error creating file");
+
+        let file_text = read_file(path).expect("Error reading file");
+
+        remove_file(path).expect("Error removing file");
+
+        assert_eq!(file_text, text);
+    }
+
+    #[test]
+    fn read_file_panics_with_incorrect_file() {
+        let path = "test_file_invalid.txt";
+
+        let result = std::panic::catch_unwind(|| read_file(path));
+
+        assert!(result.unwrap().is_err());
+    }
+}
