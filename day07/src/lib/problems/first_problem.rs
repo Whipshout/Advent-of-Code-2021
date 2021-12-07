9b4b3797e114daef7cf5e6acf369375b557a058b
Eduardo Sánchez <whipshout@gmail.com>
2021-12-07 07:29:46 +0100

    day07 added

--- day07/src/lib/problems/first_problem.rs
+++ day07/src/lib/problems/first_problem.rs
@@ -1,5 +1,31 @@
+use std::str::FromStr;
+
 pub fn solve_first_problem(input: &str) -> i32 {
-    0
+    let positions: Vec<_> = input
+        .split(',')
+        .into_iter()
+        .map(|n| i32::from_str(n).unwrap())
+        .collect();
+
+    let max = positions.iter().max().unwrap();
+
+    let mut temp_fuel = 2000000000;
+
+    (0..=*max).for_each(|n| {
+        let fuel = calculate_fuel(positions.clone(), n);
+
+        if fuel < temp_fuel {
+            temp_fuel = fuel;
+        }
+    });
+
+    temp_fuel
+}
+
+fn calculate_fuel(positions: Vec<i32>, n: i32) -> i32 {
+    positions
+        .into_iter()
+        .fold(0, |sum, val| sum + ((val - n).abs()))
 }
 
 #[cfg(test)]
@@ -7,5 +33,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn solve_first_problem_works() {}
+    fn solve_first_problem_works() {
+        let input = "1101,1,29,67,1102,0,1,65,1008,65";
+        let result = solve_first_problem(input);
+
+        assert_eq!(result, 3247);
+    }
 }
--- day07/src/lib/problems/second_problem.rs
+++ day07/src/lib/problems/second_problem.rs
@@ -1,5 +1,32 @@
+use std::str::FromStr;
+
 pub fn solve_second_problem(input: &str) -> i32 {
-    0
+    let positions: Vec<_> = input
+        .split(',')
+        .into_iter()
+        .map(|n| i32::from_str(n).unwrap())
+        .collect();
+
+    let max = positions.iter().max().unwrap();
+
+    let mut temp_fuel = 2000000000;
+
+    (0..=*max).for_each(|n| {
+        let fuel = calculate_fuel(positions.clone(), n);
+
+        if fuel < temp_fuel {
+            temp_fuel = fuel;
+        }
+    });
+
+    temp_fuel
+}
+
+fn calculate_fuel(positions: Vec<i32>, n: i32) -> i32 {
+    positions.into_iter().fold(0, |sum, val| {
+        let steps = (val - n).abs();
+        sum + (0..=steps).sum::<i32>()
+    })
 }
 
 #[cfg(test)]
@@ -8,6 +35,9 @@ mod tests {
 
     #[test]
     fn solve_second_problem_works() {
-        0
+        let input = "1101,1,29,67,1102,0,1,65,1008,65";
+        let result = solve_second_problem(input);
+
+        assert_eq!(result, 1139069);
     }
 }
